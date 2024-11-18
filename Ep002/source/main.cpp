044292b16b544638668e905ae6fd37991f7242c9
Lincoln Dias <lincolnroberto820@gmail.com>
2024-11-17 23:48:33 -0300

    Episode2 code

---
+++ Ep002/source/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <concepts>
+
+template <typename T>
+requires std::integral<T>
+T add(T a, T b)
+{
+	return a + b;
+}
+
+int main(void)
+{
+	std::cout << "Hello from C++20" << std::endl;
+	std::cout << "The sum is : " << add(7, 5) << std::endl;
+
+	return 0;
+}
